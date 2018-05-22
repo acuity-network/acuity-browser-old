1b72e11f2860cfc2df62d781cff86749a1a4b10a
Jonathan Brown <jbrown@mix-blockchain.org>
2018-05-22 12:28:49 +0700

    Make title smaller.

--- src/renderer/components/PublishMixinType.vue
+++ src/renderer/components/PublishMixinType.vue
@@ -5,7 +5,7 @@
       <section class="hero is-primary">
         <div class="hero-body">
           <div class="container">
-            <h1 class="title is-1">Publish Mixin Type</h1>
+            <h1 class="title">Publish Mixin Type</h1>
           </div>
         </div>
       </section>
