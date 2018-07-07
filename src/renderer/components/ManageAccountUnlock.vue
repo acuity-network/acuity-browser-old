6f9a0301af3b943c4e59bc9811e2de22a7691954
Jonathan Brown <jbrown@mix-blockchain.org>
2018-07-08 02:17:39 +0700

    Bind password field.

--- src/renderer/components/ManageAccountUnlock.vue
+++ src/renderer/components/ManageAccountUnlock.vue
@@ -3,14 +3,14 @@
     <header class="modal-card-head">
       <p class="modal-card-title">Unlock account</p>
     </header>
-    <form v-on:submit="unlock();$parent.close();">
+    <form v-on:submit="unlock">
       <section class="modal-card-body">
         <b-field label="Password">
-          <b-input type="password" id="password" password-reveal></b-input>
+          <b-input type="password" v-model="password" password-reveal></b-input>
         </b-field>
       </section>
       <footer class="modal-card-foot">
-          <button class="button is-primary"type="button" @click="unlock();">Unlock</button>
+          <button class="button is-primary"type="button" @click="unlock">Unlock</button>
       </footer>
     </form>
   </div>
@@ -20,9 +20,14 @@
   export default {
     name: 'manage-account-unlock',
     props: ['address'],
+    data() {
+      return {
+        password
+      }
+    },
     methods: {
       unlock() {
-        this.$web3.eth.personal.unlockAccount(this.address, document.getElementById('password').value, 0)
+        this.$web3.eth.personal.unlockAccount(this.address, this.password, 0)
         .then(result => {
           this.$parent.$parent.loadAccounts()
           this.$emit('close')
