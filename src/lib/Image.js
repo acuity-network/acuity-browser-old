aaba50b1ac5fb5e4a0574d4e6444ae9d964ebbcd
Jonathan Brown <jbrown@mix-blockchain.org>
2018-08-03 12:01:22 +0700

    Use sharp to scale images.

--- src/lib/Image.js
+++ src/lib/Image.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
-const jpeg = require('jpeg-js')
-const pica = require('pica')()
+const sharp = require('sharp')
 import jpegImageProto from './jpeg-image_pb.js'
 const Base58 = require("base-58")
 import itemProto from './item_pb.js'
@@ -12,62 +10,58 @@ export default class Image {
     this.filepath = filepath
   }
 
-  scaleImage(rawImageData, width, height) {
-    return pica.resizeBuffer({
-      src: rawImageData.data,
-      width: rawImageData.width,
-      height: rawImageData.height,
-      toWidth: width,
-      toHeight: height
+  scaleImage(source, width, height) {
+    return source
+    .resize(width, height, {fastShrinkOnLoad: false})
+    .jpeg({
+      quality: 70,
+      progressive: true,
+      chromaSubsampling: '4:4:4'
     })
-    .then(result => {
-      rawImageData = {
-        data: result,
-        width: width,
-        height: height
-      }
-      var jpegImageData = jpeg.encode(rawImageData, 70)
-
-      var data = new FormData()
-      data.append('', new File([jpegImageData.data], {type: 'application/octet-stream'}))
-
-      return this.vue.$http.post('http://127.0.0.1:5001/api/v0/add', data)
+    .toBuffer()
+    .then(data => {
+      var formData = new FormData()
+      // See https://github.com/electron/electron/issues/11700
+      formData.append('', new File([data.buffer.slice(0)], {type: 'application/octet-stream'}))
+      return this.vue.$http.post('http://127.0.0.1:5001/api/v0/add', formData)
     })
   }
 
   createMixin() {
-    var jpegData = fs.readFileSync(this.filepath)
-    var rawImageData = jpeg.decode(jpegData)
+    var source = sharp(this.filepath)
 
-    var mipmaps = []
-    var level = 1
-    do {
-      var scale = Math.pow(2, level)
-      var width = Math.floor(rawImageData.width / scale)
-      var height = Math.floor(rawImageData.height / scale)
-      console.log(level, width, height)
-      mipmaps.push(this.scaleImage(rawImageData, width, height))
-      level++
-    }
-    while (width > 64 && height > 64)
+    return source.metadata()
+    .then(metadata => {
+      var mipmaps = []
+      var level = 1
+      do {
+        var scale = Math.pow(2, level)
+        var width = Math.floor(metadata.width / scale)
+        var height = Math.floor(metadata.height / scale)
+        console.log(level, width, height)
+        mipmaps.push(this.scaleImage(source, width, height))
+        level++
+      }
+      while (width > 64 && height > 64)
 
-    return Promise.all(mipmaps)
-    .then(mipmaps => {
-      var imageMessage = new jpegImageProto.JpegMipmap()
-      imageMessage.setWidth(rawImageData.width)
-      imageMessage.setHeight(rawImageData.height)
-      mipmaps.forEach(mipmap => {
-        var mipmapLevelMessage = new jpegImageProto.MipmapLevel()
-        mipmapLevelMessage.setFilesize(mipmap.data.Size)
-        mipmapLevelMessage.setIpfsHash(Base58.decode(mipmap.data.Hash))
-        imageMessage.addMipmapLevel(mipmapLevelMessage)
+      return Promise.all(mipmaps)
+      .then(mipmaps => {
+        var imageMessage = new jpegImageProto.JpegMipmap()
+        imageMessage.setWidth(metadata.width)
+        imageMessage.setHeight(metadata.height)
+        mipmaps.forEach(mipmap => {
+          var mipmapLevelMessage = new jpegImageProto.MipmapLevel()
+          mipmapLevelMessage.setFilesize(mipmap.data.Size)
+          mipmapLevelMessage.setIpfsHash(Base58.decode(mipmap.data.Hash))
+          imageMessage.addMipmapLevel(mipmapLevelMessage)
+        })
+
+        var mixinMessage = new itemProto.Mixin()
+        mixinMessage.setMixinId(0x12745469)
+        mixinMessage.setPayload(imageMessage.serializeBinary())
+
+        return mixinMessage
       })
-
-      var mixinMessage = new itemProto.Mixin()
-      mixinMessage.setMixinId(0x12745469)
-      mixinMessage.setPayload(imageMessage.serializeBinary())
-
-      return mixinMessage
     })
   }
 }
