5dee5fb9ac277a293f792d295f183c1872acc979
Jonathan Brown <jbrown@mix-blockchain.org>
2018-08-03 15:00:44 +0700

    Support more image file types.

--- src/renderer/components/PublishImage.vue
+++ src/renderer/components/PublishImage.vue
@@ -51,7 +51,7 @@
         const {dialog} = require('electron').remote
         dialog.showOpenDialog({
           title: 'Choose image',
-          filters: [{name: 'Images', extensions: ['jpg', 'png', 'gif']}]
+          filters: [{name: 'Images', extensions: ['webp', 'jpg', 'jpeg', 'png', 'gif', 'tiff', 'svg', 'svgz', 'ppm']}]
         }, (fileNames) => {
           window.fileNames = fileNames
         })
