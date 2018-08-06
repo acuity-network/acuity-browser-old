845b099401e2d9e14b3dbeaa942711e9b4f4f8f5
Jonathan Brown <jbrown@mix-blockchain.org>
2018-08-06 16:57:10 +0700

    Add info to node status page.

--- src/renderer/components/NodeStatus.vue
+++ src/renderer/components/NodeStatus.vue
@@ -12,7 +12,24 @@
 
       <section class="section">
         <div class="container">
-
+          <b-field label="Web3 version">
+            {{ web3Version }}
+          </b-field>
+          <b-field label="Protocol version">
+            {{ protocolVersion }}
+          </b-field>
+          <b-field label="Network ID">
+            {{ networkId }}
+          </b-field>
+          <b-field label="Block number">
+            {{ blockNumber }}
+          </b-field>
+          <b-field label="Peer count">
+            {{ peerCount }}
+          </b-field>
+          <b-field label="Syncing">
+            {{ isSyncing }}
+          </b-field>
         </div>
       </section>
     </main>
@@ -22,6 +39,53 @@
 <script>
   export default {
     name: 'node-status',
-    components: {}
+    data() {
+      return {
+        web3Version: '',
+        protocolVersion: '',
+        networkId: '',
+        blockNumber: '',
+        peerCount: '',
+        isSyncing: '',
+      }
+    },
+    methods: {
+      loadData() {
+        this.$web3.eth.getBlockNumber()
+        .then(blockNumber => {
+          this.blockNumber = blockNumber.toLocaleString()
+        })
+
+        this.$web3.eth.isSyncing()
+        .then(isSyncing => {
+          this.isSyncing = isSyncing
+        })
+
+        this.$web3.eth.net.getPeerCount()
+        .then(peerCount => {
+          this.peerCount = peerCount
+        })
+      }
+    },
+    created() {
+      this.web3Version = this.$web3.version
+
+      this.$web3.eth.getProtocolVersion()
+      .then(protocolVersion => {
+        this.protocolVersion = this.$web3.utils.hexToNumber(protocolVersion)
+      })
+
+      this.$web3.eth.net.getId()
+      .then(networkId => {
+        this.networkId = networkId
+      })
+
+      this.$web3.eth.subscribe('newBlockHeaders')
+      .on('data', block => {
+        this.loadData()
+      })
+
+      this.loadData()
+    },
   }
 </script>
