0d7a35e9bf2ac9a75dcaab63ae3c3197c5985aa2
Jonathan Brown <jbrown@mix-blockchain.org>
2018-09-08 11:55:36 +0700

    Add page component.

---
+++ src/renderer/components/Page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <section class="section is-primary">
+        <h1 class="title"><slot name="title"></slot></h1>
+        <h2 class="subtitle"><slot name="subtitle"></slot></h2>
+      </section>
+
+      <section class="section">
+        <slot name="body"></slot>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'page',
+  }
+</script>
