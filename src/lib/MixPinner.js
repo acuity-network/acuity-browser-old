12e768cb07b82a90aa904a78ccaf3120a04dd8e2
Jonathan Brown <jbrown@mix-blockchain.org>
2019-01-31 00:09:25 +0700

    Improve IPFS pinning.

--- src/lib/MixPinner.js
+++ src/lib/MixPinner.js
@@ -1,11 +1,35 @@
 import MixAccount from '../lib/MixAccount.js'
 import MixItem from '../lib/MixItem.js'
+import MixContent from '../lib/MixContent.js'
+import jpegImageProto from './jpeg-image_pb.js'
 let multihashes = require('multihashes')
+let Base58 = require("base-58")
 
 export default class MixPinner {
 
   constructor(vue) {
     this.vue = vue
+  }
+
+  async pinIpfsHash(ipfsHash) {
+    let encodedIpfsHash = multihashes.toB58String(multihashes.encode(Buffer.from(ipfsHash.substr(2), "hex"), 'sha2-256'))
+    // Wait for response before continuing to try to avoid DOSing IPFS daemon.
+    await this.vue.$http.get('http://127.0.0.1:5001/api/v0/pin/add?arg=' + encodedIpfsHash)
+
+    let content = new MixContent(this.vue)
+    await content.load(ipfsHash)
+    let payloads = content.getPayloads('0x12745469')
+
+    for (let payload of payloads) {
+      let imageMessage = new jpegImageProto.JpegMipmap.deserializeBinary(payload)
+      let mipmapList = imageMessage.getMipmapLevelList()
+
+      for (let mipmap of mipmapList) {
+        let encodedIpfsHash = Base58.encode(mipmap.getIpfsHash())
+        // Wait for response before continuing to try to avoid DOSing IPFS daemon.
+        await this.vue.$http.get('http://127.0.0.1:5001/api/v0/pin/add?arg=' + encodedIpfsHash)
+      }
+    }
   }
 
   async start() {
@@ -33,7 +57,6 @@ export default class MixPinner {
     catch (e) {}
     let toBlock = await this.vue.$web3.eth.getBlockNumber()
 
-
     // Find past events.
     let events = await this.vue.$itemStoreIpfsSha256.getPastEvents('allEvents', {
       fromBlock: fromBlock,
@@ -43,10 +66,7 @@ export default class MixPinner {
 
     // Pin IPFS files.
     for (let event of events) {
-      let encodedIpfsHash = multihashes.toB58String(multihashes.encode(Buffer.from(event.returnValues.ipfsHash.substr(2), "hex"), 'sha2-256'))
-      console.log('Pinning ' + encodedIpfsHash)
-      // Wait for response before continuing to try to avoid DOSing IPFS daemon.
-      await this.vue.$http.get('http://127.0.0.1:5001/api/v0/pin/add?arg=' + encodedIpfsHash)
+      await this.pinIpfsHash(event.returnValues.ipfsHash)
     }
 
     // Store where we got to.
@@ -54,16 +74,14 @@ export default class MixPinner {
 
     // Start watching.
     this.vue.$itemStoreIpfsSha256.events.allEvents({
-      fromBlock: toBlock,
+      fromBlock: toBlock + 1,
       toBlock: 'pending',
       topics: ['0x0dbe5761780bd3332f4349220012aa42519557f909b3bfa059ada108fd6b6561',, Array.from(this.accountsToWatch)], // PublishRevision
     })
-    .on('data', async log => {
-      let encodedIpfsHash = multihashes.toB58String(multihashes.encode(Buffer.from(log.returnValues.ipfsHash.substr(2), "hex"), 'sha2-256'))
-      // Wait for response before continuing to try to avoid DOSing IPFS daemon.
-      await this.vue.$http.get('http://127.0.0.1:5001/api/v0/pin/add?arg=' + encodedIpfsHash)
+    .on('data', async event => {
+      await this.pinIpfsHash(event.returnValues.ipfsHash)
       // Store where we got to.
-      this.vue.$db.put('/pinBlock', log.blockNumber)
+      this.vue.$db.put('/pinBlock', event.blockNumber)
     })
     .on('changed', log => {
     })
