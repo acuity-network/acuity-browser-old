420fa63c30f737789a54f52c5d3355b8b9fec3b0
ethericsol <bheishman@protonmail.com>
2019-03-28 14:42:19 -0400

    Notification when receiving tx

--- src/lib/notifications.js
+++ src/lib/notifications.js
@@ -12,6 +12,14 @@ module.exports = {
         title: "Insufficient Funds",
         body: "You must fund your Mix Account before doing this.",
         icon: path.join(__dirname, '../main/AcuityWebclip.png')
+    },
+
+    mixReceived: (account, amount) => {
+        return ({
+            title: "Mix Received",
+            body: "Account " + account + " received " + amount + " MIX.",
+            icon: path.join(__dirname, '../main/AcuityWebclip.png')
+        })
     }
 
 }
