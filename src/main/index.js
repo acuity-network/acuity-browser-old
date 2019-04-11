370cc121ba0ea03118206f39d95c7e57ff3491c1
Jonathan Brown <jbrown@mix-blockchain.org>
2019-04-11 11:13:55 +0700

    Automatically close app when all windows closed on apple.

--- src/main/index.js
+++ src/main/index.js
@@ -116,12 +116,6 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
