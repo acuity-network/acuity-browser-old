7976778a8b62eb4f8f348b4a3404694ae95f37ad
Jonathan Brown <jbrown@mix-blockchain.org>
2019-04-17 17:17:29 +0700

    Add IPFS info to node status.

--- src/renderer/components/NodeStatus.vue
+++ src/renderer/components/NodeStatus.vue
@@ -5,42 +5,66 @@
     </template>
 
     <template slot="body">
-      <div v-if="downloadingParity === true">
-        <b-field label="Parity downloading">
-          <progress-bar size="tiny" :val="parityDownloadProgress" max="1000" bar-transition="none" />
-        </b-field>
-      </div>
-      <div v-if="downloadingParity === false">
-        <b-field label="Web3 version">
-          {{ web3Version }}
-        </b-field>
-        <b-field label="Protocol version">
-          {{ protocolVersion }}
-        </b-field>
-        <b-field label="Network ID">
-          {{ networkId }}
-        </b-field>
-        <b-field label="Block number">
-          {{ blockNumber }}
-        </b-field>
-        <b-field label="Peer count">
-          {{ peerCount }}
-        </b-field>
-        <b-field label="Catching up">
-          <div v-if="isSyncing">
-            <progress-bar size="tiny" :val="syncProgress" :max="syncTotal" bar-transition="none" />
+      <div class="columns">
+        <div class="column">
+          <h2 class="subtitle">MIX Blockchain</h2>
+          <div v-if="downloadingParity === true">
+            <b-field label="Parity downloading">
+              <progress-bar size="tiny" :val="parityDownloadProgress" max="1000" bar-transition="none" />
+            </b-field>
           </div>
-          <div v-else>
-            no
+          <div v-if="downloadingParity === false">
+            <b-field label="Web3 version">
+              {{ web3Version }}
+            </b-field>
+            <b-field label="Protocol version">
+              {{ protocolVersion }}
+            </b-field>
+            <b-field label="Network ID">
+              {{ networkId }}
+            </b-field>
+            <b-field label="Block number">
+              {{ blockNumber }}
+            </b-field>
+            <b-field label="Peer count">
+              {{ peerCount }}
+            </b-field>
+            <b-field label="Catching up">
+              <div v-if="isSyncing">
+                <progress-bar size="tiny" :val="syncProgress" :max="syncTotal" bar-transition="none" />
+              </div>
+              <div v-else>
+                no
+              </div>
+            </b-field>
+            <b-field label="Clock synced">
+              <span v-if="isClockSync">yes</span>
+              <span v-else>no</span>
+            </b-field>
+            <b-field label="Time drift">
+              {{ timeDrift }} ms
+            </b-field>
           </div>
-        </b-field>
-        <b-field label="Clock synced">
-          <span v-if="isClockSync">yes</span>
-          <span v-else>no</span>
-        </b-field>
-        <b-field label="Time drift">
-          {{ timeDrift }} ms
-        </b-field>
+        </div>
+        <div class="column">
+          <h2 class="subtitle">IPFS</h2>
+          <b-field label="Agent">
+            {{ ipfsAgent }}
+          </b-field>
+          <b-field label="Protocol">
+            {{ ipfsProtocol }}
+          </b-field>
+          <b-field label="Addresses">
+            <ul>
+              <li v-for="address in ipfsAddresses">
+                {{ address }}
+              </li>
+            </ul>
+          </b-field>
+          <b-field label="Peer count">
+            {{ ipfsPeerCount }}
+          </b-field>
+        </div>
       </div>
     </template>
   </page>
@@ -75,6 +99,10 @@
         highestBlock: 0,
         syncTotal: 0,
         syncProgress: 0,
+        ipfsAgent: '',
+        ipfsProtocol: '',
+        ipfsAddresses: [],
+        ipfsPeerCount: '',
       }
     },
     methods: {
@@ -129,6 +157,18 @@
         }
 
         this.peerCount = await this.$web3.eth.net.getPeerCount()
+        let ipfsId = await this.$http.get('http://127.0.0.1:5001/api/v0/id')
+        this.ipfsAgent = ipfsId.data.AgentVersion
+        this.ipfsProtocol = ipfsId.data.ProtocolVersion
+
+        let addresses = []
+        for (let address of ipfsId.data.Addresses) {
+          addresses.push(address.split('/ipfs/')[0])
+        }
+        this.ipfsAddresses = addresses;
+
+        let peers = await this.$http.get('http://127.0.0.1:5001/api/v0/swarm/peers')
+        this.ipfsPeerCount = peers.data.Peers.length
       }
     },
     async created() {
