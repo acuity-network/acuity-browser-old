1742b5d2fb7aac344f84c06ea957ee671ccb6a6a
Jonathan Brown <jbrown@mix-blockchain.org>
2019-04-30 11:02:02 +0700

    Improve accounts page.

--- src/renderer/components/ManageAccounts.vue
+++ src/renderer/components/ManageAccounts.vue
@@ -15,15 +15,6 @@
           <b-table-column field="balance" label="Balance">
             {{ props.row.balance }}
           </b-table-column>
-<!--
-          <b-table-column field="lock" label="">
-            <span class="clickable" v-if="props.row.unlocked" v-on:click="lock" :data-address="props.row.account">lock</span>
-            <span class="clickable" v-else v-on:click="unlock" :data-address="props.row.account">unlock</span>
-          </b-table-column>
--->
-          <b-table-column field="manage" label=" ">
-            <router-link :to="{ name: 'manage-account-activate', params: { controllerAddress: props.row.account }}">activate</router-link>
-          </b-table-column>
 
         </template>
         <template slot="footer">
@@ -39,14 +30,12 @@
   import Page from './Page.vue'
   import MixAccount from '../../lib/MixAccount.js'
   import MixItem from '../../lib/MixItem.js'
-  import ManageAccountUnlock from './ManageAccountUnlock.vue'
   import ManageAccountsNew from './ManageAccountsNew.vue'
 
   export default {
     name: 'manage-accounts',
     components: {
       Page,
-      ManageAccountUnlock,
       ManageAccountsNew,
     },
     data() {
@@ -62,33 +51,15 @@
           'lt': '/account/controllerAddress/z',
         })
         .on('data', async address => {
-          let result = await Promise.all([
-            this.$web3.eth.getBalance(address),
-            true,
-            new MixAccount(this.$root, address).init()
-            .then(account => {
-              return account.call(this.$accountProfile.methods.getProfile())
-            })
-            .then(itemId => {
-              return new MixItem(this, itemId).init()
-            })
-            .then(item => {
-              return item.latestRevision().load()
-            })
-            .then(revision => {
-              return revision.getTitle()
-            })
-            .catch(() => {
-              return false
-            }),
-          ])
+          let account = await new MixAccount(this.$root, address).init()
+          let itemId = await account.call(this.$accountProfile.methods.getProfile())
+          let item = await new MixItem(this, itemId).init()
+          let revision = await item.latestRevision().load()
+
           var row = {
             account: address,
-            name: result[2] ? result[2] : address,
-            balance: this.$web3.utils.fromWei(result[0]),
-            unlocked: result[1],
-            action: (result[1] == false) ? 'unlock' : ((result[2] == false) ? 'deploy' : 'lock'),
-            route: (result[1] == false) ? 'manage-account-unlock' : 'manage-account-controller',
+            name: revision.getTitle(),
+            balance: this.$web3.utils.fromWei(await account.getUnconfirmedBalance()),
           }
           this.data.push(row)
           if (window.activeAccount && address == window.activeAccount.controllerAddress) {
@@ -99,25 +70,6 @@
       async select(event) {
         let account = await new MixAccount(this.$root, event.account).init()
         account.select()
-      },
-      unlock(event) {
-        this.$modal.open({
-          parent: this,
-          component: ManageAccountUnlock,
-          hasModalCard: true,
-          props: {
-            address: event.target.dataset.address,
-          },
-        })
-      },
-      lock(event) {
-        this.$web3.eth.personal.lockAccount(event.target.dataset.address)
-        .then(result => {
-          this.loadAccounts();
-        })
-        .catch(error => {
-          console.log(error)
-        })
       },
     },
     created() {
