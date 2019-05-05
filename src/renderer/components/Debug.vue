0f4f88fe57794eb2499a32643d2c1b6e88800d64
Jonathan Brown <jbrown@mix-blockchain.org>
2019-05-05 20:56:24 +0700

    Debug with enter key.

--- src/renderer/components/Debug.vue
+++ src/renderer/components/Debug.vue
@@ -6,7 +6,7 @@
 
     <template slot="body">
       <b-field :label="$t('itemId')">
-        <b-input id="itemId" autocomplete="off" inputmode="verbatim" placeholder="0x0000000000000000000000000000000000000000000000000000000000000000" spellcheck="false" size="66" style="font-family: monospace;"></b-input>
+        <b-input id="itemId" @keydown.native.enter="read" autocomplete="off" inputmode="verbatim" placeholder="0x0000000000000000000000000000000000000000000000000000000000000000" spellcheck="false" size="66" style="font-family: monospace;"></b-input>
       </b-field>
 
       <button class="button is-primary" @click="read">{{ $t('readItem') }}</button>
