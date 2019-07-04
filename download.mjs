ab18a000a3be8b3078ce07aa83554aab4750e70b
Jonathan Brown <jbrown@mix-blockchain.org>
2019-07-04 22:24:37 +0700

    Parity 2.4.9

--- download.mjs
+++ download.mjs
@@ -6,24 +6,24 @@ import download from 'download'
 
 let urls = {
 	linux: {
-		parity: 'https://releases.parity.io/ethereum/v2.4.8/x86_64-unknown-linux-gnu/parity',
+		parity: 'https://releases.parity.io/ethereum/v2.4.9/x86_64-unknown-linux-gnu/parity',
 		ipfs: 'https://github.com/ipfs/go-ipfs/releases/download/v0.4.21/go-ipfs_v0.4.21_linux-amd64.tar.gz',
 		ethminer: 'https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1/ethminer-0.17.1-linux-x86_64.tar.gz',
 	},
 	darwin: {
-		parity: 'https://releases.parity.io/ethereum/v2.4.8/x86_64-apple-darwin/parity',
+		parity: 'https://releases.parity.io/ethereum/v2.4.9/x86_64-apple-darwin/parity',
 		ipfs: 'https://github.com/ipfs/go-ipfs/releases/download/v0.4.21/go-ipfs_v0.4.21_darwin-amd64.tar.gz',
 		ethminer: 'https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1/ethminer-0.17.1-darwin-x86_64.tar.gz',
 	},
 	win32: {
-		parity: 'https://releases.parity.io/ethereum/v2.4.8/x86_64-pc-windows-msvc/parity.exe',
+		parity: 'https://releases.parity.io/ethereum/v2.4.9/x86_64-pc-windows-msvc/parity.exe',
 		ipfs: 'https://github.com/ipfs/go-ipfs/releases/download/v0.4.21/go-ipfs_v0.4.21_windows-amd64.zip',
 		ethminer: 'https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1/ethminer-0.17.1-cuda10.0-windows-amd64.zip',
 	},
 }
 
 try {
-	if (parseInt(fs.readFileSync('download_rev')) >= 2) {
+	if (parseInt(fs.readFileSync('download_rev')) >= 3) {
 		process.exit(0)
 	}
 } catch (e) {}
@@ -45,7 +45,7 @@ let ethminer = download(archUrls['ethminer'], path.join('static', 'ethminer'), {
 
 Promise.all([parity, ipfs, ethminer])
 .then(() => {
-	fs.writeFileSync('download_rev', '2')
+	fs.writeFileSync('download_rev', '3')
 	process.exit(0)
 })
 .catch(e => {
