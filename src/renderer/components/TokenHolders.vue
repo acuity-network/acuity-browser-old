9fbf980f797434d4720a7c6ad6d30ea321faa771
Jonathan Brown <jbrown@mix-blockchain.org>
2019-08-14 11:23:58 +0800

    Fix token holder list.

--- src/renderer/components/TokenHolders.vue
+++ src/renderer/components/TokenHolders.vue
@@ -1,8 +1,8 @@
 <template>
-  <b-table :data="data" default-sort="amount" default-sort-direction="desc">
+  <b-table :data="data" default-sort="amount" default-sort-direction="asc">
     <template slot-scope="props">
       <b-table-column field="holder" :label="$t('holder')" sortable>
-        <profile-link :address="props.row.holder"></profile-link>
+        <profile-link :address="props.row.holder" :key="props.row.holder"></profile-link>
       </b-table-column>
 
       <b-table-column field="amount" :label="$t('amount')" numeric sortable>
@@ -35,12 +35,14 @@
       this.token = new this.$mixClient.web3.eth.Contract(require('../../lib/contracts/CreatorToken.abi.json'), this.address)
 
       let accountBalances = await this.token.methods.getAccountBalances().call()
+      let data = []
       for (let i in accountBalances.accounts) {
-        this.data.push({
+        data.push({
           holder: accountBalances.accounts[i],
           amount: this.$mixClient.web3.utils.fromWei(this.$mixClient.web3.utils.toBN(accountBalances.balances[i])),
         })
       }
+      this.data = data
     },
   }
 </script>
