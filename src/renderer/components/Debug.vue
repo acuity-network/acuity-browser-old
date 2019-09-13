6335110454b1abb20f952383567115fb19c93677
Jonathan Brown <jbrown@mix-blockchain.org>
2019-09-13 12:29:18 +0800

    Make Debug.vue translatable.

--- src/renderer/components/Debug.vue
+++ src/renderer/components/Debug.vue
@@ -1,15 +1,15 @@
 <template>
   <page>
     <template slot="title">
-      {{ $t('debugItem') }}
+      {{ $t('Debug.DebugItem') }}
     </template>
 
     <template slot="body">
-      <b-field :label="$t('itemId')" :message="message">
-        <b-input v-model="itemId" @keydown.native.enter="read" autocomplete="off" inputmode="verbatim" placeholder="0x0000000000000000000000000000000000000000000000000000000000000000" spellcheck="false" size="66" style="font-family: monospace;"></b-input>
+      <b-field label="itemId">
+        <b-input v-model.trim="itemId" @keydown.native.enter="read" autocomplete="off" inputmode="verbatim" placeholder="0x0000000000000000000000000000000000000000000000000000000000000000" spellcheck="false" size="66" style="font-family: monospace;"></b-input>
       </b-field>
-      <button class="button is-primary" @click="read">{{ $t('readItem') }}</button>
-      <code id="output" style="display: block; white-space: pre;"></code>
+      <button class="button is-primary" @click="read">{{ $t('Debug.ReadItem') }}</button>
+      <code v-html="output" style="display: block; white-space: pre;"></code>
     </template>
   </page>
 </template>
@@ -30,6 +30,11 @@
   import MixItem from '../../lib/MixItem.js'
   import setTitle from '../../lib/setTitle.js'
 
+  // https://github.com/jasonmoo/t.js/blob/master/t.js
+  function scrub(val) {
+		return new Option(val).innerHTML
+	}
+
   export default {
     name: 'debug',
     components: {
@@ -38,11 +43,11 @@
     data() {
       return {
         itemId: '',
-        message: '',
+        output: '',
       }
     },
     created() {
-      setTitle(this.$t('debugItem'))
+      setTitle(this.$t('Debug.DebugItem'))
       let clipboardText: string = clipboard.readText()
       if (this.$mixClient.web3.utils.isHexStrict(clipboardText) && clipboardText.length == 66) {
         this.itemId = clipboardText
@@ -50,127 +55,121 @@
     },
     methods: {
       async read(event) {
-        let output = document.getElementById('output')
-        output.innerHTML = ''
-
-        this.itemId = this.itemId.trim()
-        try {
-          await new MixItem(this.$root, this.itemId).init()
-        }
-        catch (e) {
-          this.message = 'Item not found.'
-          return
-        }
-
-        this.message = ''
+        this.output = ''
 
         let shortId = await this.$mixClient.itemStoreShortId.methods.getShortId(this.itemId).call()
-        output.appendChild(document.createTextNode('shortId: '  + shortId + '\n'))
+        this.output += 'shortId: '  + shortId + '\n'
 
-        let itemStoreAddress = await this.$mixClient.itemStoreRegistry.methods.getItemStore(this.itemId).call()
-        output.appendChild(document.createTextNode('itemStoreAddress: '  + itemStoreAddress + '\n'))
+        let itemStoreAddress
+        try {
+          itemStoreAddress = await this.$mixClient.itemStoreRegistry.methods.getItemStore(this.itemId).call()
+        }
+        catch (e) {
+          this.output += this.$t('Debug.itemStoreNotFound') + '\n'
+          return
+        }
+        this.output += 'itemStoreAddress: '  + itemStoreAddress + '\n'
 
         let itemStoreAbi = require('../../lib/contracts/MixItemStoreInterface.abi.json')
         let itemStore = new this.$mixClient.web3.eth.Contract(itemStoreAbi, itemStoreAddress)
 
         let inUse = await itemStore.methods.getInUse(this.itemId).call()
         if (!inUse) {
-          output.append('Item not found.\n')
+          this.output += this.$t('Debug.ItemNotFound') + '\n'
           return
         }
 
         let contractId = await itemStore.methods.getContractId().call()
         if (contractId != "0xf1b5847865d2094d") {
-          output.append('Unknown item store.\n')
+          this.output += this.$t('Debug.UnsupportedItemStore') + '\n'
           return
         }
-        output.append('itemStore: ItemStoreIpfsSha256\n')
+        this.output += 'itemStore: ItemStoreIpfsSha256\n'
 
         let item = await this.$mixClient.itemStoreIpfsSha256.methods.getItem(this.itemId).call()
-        output.append('Updatable: ' + ((item.flags & 0x01) ? 'true' : 'false') + '\n')
-        output.append('Enforce revisions: ' + ((item.flags & 0x02) ? 'true' : "false") + '\n')
-        output.append('Retractable: ' + ((item.flags & 0x04) ? 'true' : 'false') + '\n')
-        output.append('Transferable: ' + ((item.flags & 0x08) ? 'true' : 'false') + '\n')
-        output.append('Owner: ' + item.owner + '\n')
-        output.append('Revision count: ' + item.ipfsHashes.length + '\n')
+        this.output += this.$t('Debug.Updatable') + ': ' + ((item.flags & 0x01) ? 'true' : 'false') + '\n' +
+          this.$t('Debug.EnforceRevisions') + ': ' + ((item.flags & 0x02) ? 'true' : "false") + '\n' +
+          this.$t('Debug.Retractable') + ': ' + ((item.flags & 0x04) ? 'true' : 'false') + '\n' +
+          this.$t('Debug.Transferable') + ': ' + ((item.flags & 0x08) ? 'true' : 'false') + '\n' +
+          this.$t('Debug.Owner') + ': ' + item.owner + '\n' +
+          this.$t('Debug.RevisionCount') + ': ' + item.ipfsHashes.length + '\n'
 
         for (let i = 0; i < item.ipfsHashes.length; i++) {
+          this.output += '\n' + this.$t('Debug.Revision') + '  ' + i + '\n'
+
           let timestamp = new Date(item.timestamps[i] * 1000)
-          output.append('\nRevision ' + i + ' timestamp: ' + timestamp + '\n')
+          this.output += this.$t('Debug.Timestamp') + ': ' + timestamp + '\n'
 
           let ipfsHash = multihashes.toB58String(multihashes.encode(Buffer.from(item.ipfsHashes[i].substr(2), "hex"), 'sha2-256'))
-          output.append('Revision ' + i + ' IPFS hash: ' + ipfsHash + '\n')
+          this.output += this.$t('Debug.IpfsHash') + ': ' + ipfsHash + '\n'
 
           let response = await this.$ipfsClient.get('cat?arg=/ipfs/' + ipfsHash, false)
           let containerPayload = Buffer.from(response, "binary")
-          output.append('Compressed length: ' + formatByteCount(containerPayload.length) + '\n')
+          this.output += this.$t('Debug.CompressedLength') + ': ' + formatByteCount(containerPayload.length) + '\n'
 
           let itemPayload = await brotli.decompress(Buffer.from(containerPayload))
-          output.append('Uncompressed length: ' + formatByteCount(itemPayload.length) + '\n')
+          this.output += this.$t('Debug.UncompressedLength') + ': ' + formatByteCount(itemPayload.length) + '\n'
 
           let itemMessage = ItemProto.Item.deserializeBinary(itemPayload)
           let mixins = itemMessage.getMixinPayloadList()
 
-          output.append('Mixin count: ' + mixins.length + '\n')
+          this.output += this.$t('Debug.MixinCount') + ': ' + mixins.length + '\n'
 
           for (let i = 0; i < mixins.length; i++) {
-            output.append('\nMixin ' + i + '\n')
+            this.output += '\nMixin ' + i + '\n'
             let mixinId = '0x' + ('00000000' + mixins[i].getMixinId().toString(16)).slice(-8)
-            output.append('mixinId: ' + mixinId + '\n')
+            this.output += 'mixinId: ' + mixinId + '\n'
 
             let mixinPayload = mixins[i].getPayload()
 
             switch (mixinId) {
               case '0x51c32e3a':
-                output.append('Mixin type: Mixin type\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.MixinType') + '\n'
                 break
 
               case '0x9bc7a0e6':
-                output.append('Mixin type: Language\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.LanguageTag') + '\n'
                 let languageMessage = LanguageMixinProto.LanguageMixin.deserializeBinary(mixinPayload)
-                output.appendChild(document.createTextNode('Language tag: '  + languageMessage.getLanguageTag() + '\n'))
+                this.output += this.$t('Debug.LanguageTag') + ': '  + scrub(languageMessage.getLanguageTag()) + '\n'
                 break
 
               case '0x344f4812':
-                output.append('Mixin type: Title\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.Title') + '\n'
                 let titleMessage = TitleMixinProto.TitleMixin.deserializeBinary(mixinPayload)
-                output.appendChild(document.createTextNode('Title: '  + titleMessage.getTitle() + '\n'))
+                this.output += this.$t('Debug.Title') + ': '  + scrub(titleMessage.getTitle()) + '\n'
                 break
 
               case '0x2d382044':
-                output.append('Mixin type: Body text\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.BodyText') + '\n'
                 let bodyTextMessage = BodyTextMixinProto.BodyTextMixin.deserializeBinary(mixinPayload)
-                output.appendChild(document.createTextNode('Body text:\n'  + bodyTextMessage.getBodyText() + '\n'))
+                this.output += this.$t('Debug.BodyText') + ':\n'  + scrub(bodyTextMessage.getBodyText()) + '\n'
                 break
 
               case '0xcdce4e5d':
-                output.append('Mixin type: Mixin Schema\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.MixinSchema') + '\n'
                 let mixinSchemaMessage = MixinSchemaMixinProto.MixinSchemaMixin.deserializeBinary(mixinPayload)
-                output.appendChild(document.createTextNode('Mixin schema:\n'  + mixinSchemaMessage.getMixinSchema() + '\n'))
+                this.output += this.$t('Debug.MixinSchema') + ':\n'  + scrub(mixinSchemaMessage.getMixinSchema()) + '\n'
                 break
 
               case '0x045eee8c':
-                output.append('Mixin type: Image\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.Image') + '\n'
                 let imageMessage = ImageMixinProto.ImageMixin.deserializeBinary(mixinPayload)
                 let width = imageMessage.getWidth()
-                output.append('Original width: ' + width + '\n')
+                this.output += this.$t('Debug.OriginalWidth') + ': ' + width + '\n'
                 let height = imageMessage.getHeight()
-                output.append('Original height: ' + height + '\n')
+                this.output += this.$t('Debug.OriginalHeight') + ': ' + height + '\n'
                 let mipmaps = imageMessage.getMipmapLevelList()
-                output.append('Mipmap levels: ' + mipmaps.length + '\n')
+                this.output += this.$t('Debug.MipmapLevels') + ': ' + mipmaps.length + '\n'
                 let renderHeight = Math.round(256 * height / width)
                 for (let j = 0; j < mipmaps.length; j++) {
-                  output.append('\nMipmap level: ' + j + '\n')
-                  output.append('Mipmap filesize: ' + formatByteCount(mipmaps[j].getFilesize()) + '\n')
-                  let el = document.createElement('img')
-                  let domString = '<img src="http://127.0.0.1:5102/ipfs/' + Base58.encode(mipmaps[j].getIpfsHash()) + '" width="256" height="' + renderHeight + '" style="display: block;">'
-                  el.innerHTML = domString
-                  output.appendChild(el.firstChild)
+                  this.output += '\n' + this.$t('Debug.MipmapLevel') + ': ' + j + '\n'
+                  this.output += this.$t('Debug.MipmapFilesize') + ': ' + formatByteCount(mipmaps[j].getFilesize()) + '\n'
+                  this.output += '<img src="http://127.0.0.1:5102/ipfs/' + Base58.encode(mipmaps[j].getIpfsHash()) + '" width="256" height="' + renderHeight + '" style="display: block;">'
                 }
                 break
 
               case '0xbcec8faa':
-                output.append('Mixin type: Topic Feed\n')
+                this.output += this.$t('Debug.MixinType') + ': ' + this.$t('Debug.TopicFeed') + '\n'
                 break
             }
           }
