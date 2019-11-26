c02a06ab8cabd551eb0ee8a6afede803efd2fa4c
Jonathan Brown <jbrown@mix-blockchain.org>
2019-11-27 00:03:19 +0700

    Simplify formatByteCount()

--- src/lib/formatByteCount.ts
+++ src/lib/formatByteCount.ts
@@ -1,9 +1,7 @@
-function formatByteCount(bytes, digits = 2) {
-	if (bytes < 1000) return bytes + ' B'
-	bytes /= 1000
+function formatByteCount(bytes) {
 	let i = Math.floor(Math.log(bytes) / Math.log(1000))
-	let units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-	return (bytes / (1000 ** i)).toFixed(digits) + ' ' + units[i]
+	let units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+	return (bytes / (1000 ** i)).toLocaleString() + ' ' + units[i]
 }
 
 export default formatByteCount
