193d0d0eb286d32c548ba0eba08764d9d5d38620
Jonathan Brown <jbrown@mix-blockchain.org>
2019-11-27 15:50:32 +0700

    Use DNS for IPFS bootnodes.

--- src/lib/web/IpfsClient.js
+++ src/lib/web/IpfsClient.js
@@ -1,24 +1,24 @@
 import Ipfs from 'ipfs'
 
 let bootnodes = [
-  '/ip4/172.104.175.158/tcp/4002/ws/ipfs/QmQ38hetbvfJwhDXvXxyxT8reydNwPq6n9eXzEB11cwsji',
-  '/ip6/2400:8901::f03c:91ff:fe46:1815/tcp/4002/ws/ipfs/QmQ38hetbvfJwhDXvXxyxT8reydNwPq6n9eXzEB11cwsji',
-  '/ip4/74.207.240.177/tcp/4002/ws/ipfs/QmTdLvqQxAexuJAbSv8MnD3gK5DcscL15WWA8sYUH4vMvi',
-  '/ip6/2600:3c01::f03c:91ff:fed5:2abf/tcp/4002/ws/ipfs/QmTdLvqQxAexuJAbSv8MnD3gK5DcscL15WWA8sYUH4vMvi',
-  '/ip4/173.255.195.214/tcp/4002/ws/ipfs/QmZDy4rjTwvkkv4CRre87Z4ohr5JfMMSNwhbdcX21szCnn',
-  '/ip6/2600:3c00::f03c:91ff:fed5:2aa3/tcp/4002/ws/ipfs/QmZDy4rjTwvkkv4CRre87Z4ohr5JfMMSNwhbdcX21szCnn',
-  '/ip4/50.116.38.52/tcp/4002/ws/ipfs/QmcR25jursru6CEBWonPaqdLHAj5Ct9LLPu7Dk41dLPaxu',
-  '/ip6/2600:3c02::f03c:91ff:fed5:2a0c/tcp/4002/ws/ipfs/QmcR25jursru6CEBWonPaqdLHAj5Ct9LLPu7Dk41dLPaxu',
-  '/ip4/45.79.128.151/tcp/4002/ws/ipfs/QmXZBgSuTxKGsYmx6N1G8EEUWAm1tRXGuYadfxhgWetJf2',
-  '/ip6/2600:3c03::f03c:91ff:fed5:2a82/tcp/4002/ws/ipfs/QmXZBgSuTxKGsYmx6N1G8EEUWAm1tRXGuYadfxhgWetJf2',
-  '/ip4/139.162.224.203/tcp/4002/ws/ipfs/QmS6XPQKZSinqwFCsth7LxJeeH66ZiAgYxPphPCoWTiefq',
-  '/ip6/2a01:7e00::f03c:91ff:fed5:2a94/tcp/4002/ws/ipfs/QmS6XPQKZSinqwFCsth7LxJeeH66ZiAgYxPphPCoWTiefq',
-  '/ip4/172.104.130.233/tcp/4002/ws/ipfs/Qmar9pZaQPaMisc1x1LstphJV1jJiPrv21Edig93bz6oh8',
-  '/ip6/2a01:7e01::f03c:91ff:fed5:2a00/tcp/4002/ws/ipfs/Qmar9pZaQPaMisc1x1LstphJV1jJiPrv21Edig93bz6oh8',
-  '/ip4/172.104.68.7/tcp/4002/ws/ipfs/QmPe3zVKqnwPyBDf51zg6XkKTLz2tx4iW3DtCtK6ojm6er',
-  '/ip6/2400:8902::f03c:91ff:fed5:2ac0/tcp/4002/ws/ipfs/QmPe3zVKqnwPyBDf51zg6XkKTLz2tx4iW3DtCtK6ojm6er',
-  '/ip4/172.105.16.240/tcp/4002/ws/ipfs/Qmex27aG4LYeArEHBViQ52KN9coEwmwwsaRNKzTSbdBMSy',
-  '/ip6/2600:3c04::f03c:91ff:fec3:c620/tcp/4002/ws/ipfs/Qmex27aG4LYeArEHBViQ52KN9coEwmwwsaRNKzTSbdBMSy',
+  '/dns4/singapore.mix-blockchain.org/tcp/4002/ws/ipfs/QmQ38hetbvfJwhDXvXxyxT8reydNwPq6n9eXzEB11cwsji',
+  '/dns6/singapore.mix-blockchain.org/tcp/4002/ws/ipfs/QmQ38hetbvfJwhDXvXxyxT8reydNwPq6n9eXzEB11cwsji',
+  '/dns4/freemont.mix-blockchain.org/tcp/4002/ws/ipfs/QmTdLvqQxAexuJAbSv8MnD3gK5DcscL15WWA8sYUH4vMvi',
+  '/dns6/freemont.mix-blockchain.org/tcp/4002/ws/ipfs/QmTdLvqQxAexuJAbSv8MnD3gK5DcscL15WWA8sYUH4vMvi',
+  '/dns4/dallas.mix-blockchain.org/tcp/4002/ws/ipfs/QmZDy4rjTwvkkv4CRre87Z4ohr5JfMMSNwhbdcX21szCnn',
+  '/dns6/dallas.mix-blockchain.org/tcp/4002/ws/ipfs/QmZDy4rjTwvkkv4CRre87Z4ohr5JfMMSNwhbdcX21szCnn',
+  '/dns4/atlanta.mix-blockchain.org/tcp/4002/ws/ipfs/QmcR25jursru6CEBWonPaqdLHAj5Ct9LLPu7Dk41dLPaxu',
+  '/dns6/atlanta.mix-blockchain.org/tcp/4002/ws/ipfs/QmcR25jursru6CEBWonPaqdLHAj5Ct9LLPu7Dk41dLPaxu',
+  '/dns4/newark.mix-blockchain.org/tcp/4002/ws/ipfs/QmXZBgSuTxKGsYmx6N1G8EEUWAm1tRXGuYadfxhgWetJf2',
+  '/dns6/newark.mix-blockchain.org/tcp/4002/ws/ipfs/QmXZBgSuTxKGsYmx6N1G8EEUWAm1tRXGuYadfxhgWetJf2',
+  '/dns4/london.mix-blockchain.org/tcp/4002/ws/ipfs/QmS6XPQKZSinqwFCsth7LxJeeH66ZiAgYxPphPCoWTiefq',
+  '/dns6/london.mix-blockchain.org/tcp/4002/ws/ipfs/QmS6XPQKZSinqwFCsth7LxJeeH66ZiAgYxPphPCoWTiefq',
+  '/dns4/frankfurt.mix-blockchain.org/tcp/4002/ws/ipfs/Qmar9pZaQPaMisc1x1LstphJV1jJiPrv21Edig93bz6oh8',
+  '/dns6/frankfurt.mix-blockchain.org/tcp/4002/ws/ipfs/Qmar9pZaQPaMisc1x1LstphJV1jJiPrv21Edig93bz6oh8',
+  '/dns4/tokyo.mix-blockchain.org/tcp/4002/ws/ipfs/QmPe3zVKqnwPyBDf51zg6XkKTLz2tx4iW3DtCtK6ojm6er',
+  '/dns6/tokyo.mix-blockchain.org/tcp/4002/ws/ipfs/QmPe3zVKqnwPyBDf51zg6XkKTLz2tx4iW3DtCtK6ojm6er',
+  '/dns4/toronto.mix-blockchain.org/tcp/4002/ws/ipfs/Qmex27aG4LYeArEHBViQ52KN9coEwmwwsaRNKzTSbdBMSy',
+  '/dns6/toronto.mix-blockchain.org/tcp/4002/ws/ipfs/Qmex27aG4LYeArEHBViQ52KN9coEwmwwsaRNKzTSbdBMSy',
 ]
 
 
@@ -49,7 +49,9 @@ export default class IpfsClient {
   		try {
         await this.node.swarm.connect(bootnode)
   		}
-  		catch (e) {}
+  		catch (e) {
+        console.error(e)
+      }
   	}
   }
 
