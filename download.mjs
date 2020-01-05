74fd43c1e11800e29e45a187a63a2ca47cfcaad9
Jonathan Brown <jbrown@mix-blockchain.org>
2020-01-06 00:21:19 +0700

    Fix build.

--- download.mjs
+++ download.mjs
@@ -19,13 +19,13 @@ let urls = {
 	darwin: {
 		parity: 'https://releases.parity.io/ethereum/v2.5.13/x86_64-apple-darwin/parity',
 		ipfs: 'https://github.com/ipfs/go-ipfs/releases/download/v0.4.22/go-ipfs_v0.4.22_darwin-amd64.tar.gz',
-    ffmpeg: 'https://ffmpeg.zeranoe.com/builds/macos64/shared/ffmpeg-4.2.2-macos64-shared.zip',
+    ffmpeg: 'https://ffmpeg.zeranoe.com/builds/macos64/shared/ffmpeg-4.2.1-macos64-shared.zip',
     youtubeDl: 'https://yt-dl.org/downloads/2020.01.01/youtube-dl',
 	},
 	win32: {
 		parity: 'https://releases.parity.io/ethereum/v2.5.13/x86_64-pc-windows-msvc/parity.exe',
 		ipfs: 'https://github.com/ipfs/go-ipfs/releases/download/v0.4.22/go-ipfs_v0.4.22_windows-amd64.zip',
-    ffmpeg: 'https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-4.2.2-win64-shared.zip',
+    ffmpeg: 'https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-4.2.1-win64-shared.zip',
     youtubeDl: 'https://yt-dl.org/downloads/2020.01.01/youtube-dl.exe',
 	},
 }
