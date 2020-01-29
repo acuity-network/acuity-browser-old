fbb35eb7ed917d6f550691be0601b64c8dbe3fb4
Jonathan Brown <jbrown@mix-blockchain.org>
2020-01-29 08:13:13 +0700

    Remove untranslated langauges.

--- src/renderer/i18n/index.js
+++ src/renderer/i18n/index.js
@@ -1,25 +1,9 @@
-import de from './de.js'
 import en_US from './en-US.js'
-import fr from './fr.js'
-import id from './id.js'
-import ja from './ja.js'
-import kr from './kr.js'
 import ru from './ru.js'
 import th from './th.js'
-import vi from './vi.js'
-import zh_Hans from './zh-Hans.js'
-import zh_Hant from './zh-Hant.js'
 
 export default {
-	'de': de,
 	'en-US': en_US,
-	'fr': fr,
-	'id': id,
-	'ja': ja,
-	'kr': kr,
 	'ru': ru,
 	'th': th,
-	'vi': vi,
-	'zh-Hans': zh_Hans,
-	'zh-Hant': zh_Hant,
 }
