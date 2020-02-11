0e8bfc3a5bd63813a326da0b2cf41b3bcc3110ff
Jonathan Brown <jbrown@mix-blockchain.org>
2020-02-11 07:28:38 +0700

    Use node's built-in brotli instead of iltorb.

--- src/lib/brotli.ts
+++ src/lib/brotli.ts
@@ -1,11 +1,11 @@
-import brotli from 'iltorb'
+import zlib from 'zlib'
 
 function compress(input) {
-  return brotli.compress(Buffer.from(input))
+  return zlib.brotliCompressSync(Buffer.from(input))
 }
 
 function decompress(input) {
-  return brotli.decompress(input)
+  return zlib.brotliDecompressSync(input)
 }
 
 export default { compress, decompress }
