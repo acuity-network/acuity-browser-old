887f06db5710ff29454fed745b385afa289ed749
Jonathan Brown <jbrown@mix-blockchain.org>
2020-03-03 22:35:43 +0700

    Add default feeds.

--- src/renderer/components/routes/Home.vue
+++ src/renderer/components/routes/Home.vue
@@ -8,6 +8,7 @@
   import Page from '../Page.vue'
   import ViewItem from './ViewItem.vue'
   import setTitle from '../../../lib/setTitle'
+  import bs58 from 'bs58'
 
   export default {
     name: 'home',
@@ -39,7 +40,28 @@
       })
 
       if (feedIds.length == 0) {
-        feedIds.push('0x86e5019a26041a2805d500f5aa135795bd907eed0126c41bf1b5847865d2094d')
+        feedIds = [
+          '0x86e5019a26041a2805d500f5aa135795bd907eed0126c41bf1b5847865d2094d',
+          '0x' + bs58.decode('CgziFUBnyJrZfDo6jM4XGpbrxPm3J5AxJ').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('CiFdy7CEpnQuhgwJY4pwnxD6CL2re527R').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('H91JVrbDyivaP5xo5obFbiWXGVRX5uL1').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('PojNbuJpgVjrjkC8csBf2HdVSWZFRZMSi').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('VkmJBvxyPg5A1KdfXBeuboWt8Tgco67Q').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('BtYDECATwEPear16xyQBvxF7zX83GC4J').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('F7tdrrFzR4L1fhn3DgtA8Bf2nsVnSkA4W').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('64Ly1YSy9NRXm8pw1zYeNcGAwWMDeEXKP').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('7tE52QZbGYQ1PtTcvYLdNkM3Fkzh1g24Q').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('HuWBRzBuUkfsx7KKrSTFXGwFux5ZwrQzL').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('FQ5LyADDgyMrAYGk5pYp987EXY1vH3Jni').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('JtiLupdkv8cxERFJRgBhQCmFH4WCUGrce').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('7ARkzMTmvgosZ18TR1CcZ3sXHF23RNxwD').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('PtfAXEGn7PXFcEQDjFVpm3gKogUe1n97r').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('8Fe7PX8JQfg8rAXKet8BT87JWvWNrMGBX').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('32qEcLW5zcthiEuYK1ai3gpRwPPc9wfcu').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('AfpJmbkbgBi6hE1h33k3ZaLMoio2e6qB2').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('95oxHN6btfcvgifVmTiz7U7ssczuu9Bqt').toString('hex') + 'f1b5847865d2094d',
+          '0x' + bs58.decode('3HY58gcHv9dEJk4cEQTm3obQUeZPnzMsT').toString('hex') + 'f1b5847865d2094d',
+        ]
       }
 
       let topicsPromise = new Promise((resolve, reject) => {
