2100aceb1f19d9d1afdb1297d62c019f98f3b21a
Jonathan Brown <jbrown@mix-blockchain.org>
2020-04-04 11:47:57 +0700

    Fix build.

--- src/renderer/shims-vue.d.ts
+++ src/renderer/shims-vue.d.ts
@@ -1,22 +1,4 @@
-import Vue from 'vue'
-
 declare module '*.vue' {
+  import Vue from 'vue'
   export default Vue
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $http: any,
-    $db: any,
-    $mixClient: any,
-    $notifications: any,
-    $settings: any,
-    $ipfsClient: any,
-    $activeAccount: any,
-    $isDesktop: boolean,
-  }
-}
-
-interface Blob {
-  arrayBuffer(): Promise<ArrayBuffer>
 }
---
+++ src/renderer/shims-vue2.d.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $http: any,
+    $db: any,
+    $mixClient: any,
+    $notifications: any,
+    $settings: any,
+    $ipfsClient: any,
+    $activeAccount: any,
+    $isDesktop: boolean,
+  }
+}
+
+interface Blob {
+  arrayBuffer(): Promise<ArrayBuffer>
+}
